a04a55eafc5e4f2af0a50ea0df4d3eb74344b2e7
Churilina <victoria.vlasova2015@gmail.com>
2022-02-20 18:12:09 +0400

    task.js modified

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,14 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let discriminant = (Math.pow(b, 2)-4*a*c);
+  if (discriminant < 0) {
+  return []; // array
+} else if (discriminant === 0) {
+  return [-b/(2*a)];
+} else if (discriminant > 0) {
+  return [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];;
 }
-
+}
+solveEquation(1, 5, 4);
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
