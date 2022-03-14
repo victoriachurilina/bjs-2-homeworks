20aa789010c8753f7eedc886ece7ae4f96be10af
Churilina <victoria.vlasova2015@gmail.com>
2022-03-14 19:48:12 +0400

    modified

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
-
-function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
-}
+  let result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+    return result; // boolean
+  }
+  console.log(compareArrays([8, 1, 2], [8, 1, 2]));
+  console.log(compareArrays([8, 9], [6]));
+  console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]));
+  console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4]));
+  console.log(compareArrays([1, 2, 3], [2, 3, 1]));
+  
+  function advancedFilter(arr) {
+  let resultArr = arr.filter((numbers) => numbers > 0).filter((numbers) => numbers % 3 === 0).map((numbers) => numbers * 10);  
+    return resultArr; // array
+  }
+  console.log(advancedFilter([-1,6,-9,3])); // [60,30]
+  console.log(advancedFilter([-10,-21,12,123])); // [120, 1230]
+  console.log(advancedFilter([-1,-2])); // []
+  
